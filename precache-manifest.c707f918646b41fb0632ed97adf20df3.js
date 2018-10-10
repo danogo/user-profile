881905bd17bec6e8d5fcf8567a1107517b846e5c
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-10-10 19:20:58 +0200

    Updates

---
+++ precache-manifest.c707f918646b41fb0632ed97adf20df3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "953434256b4f012225f8",
+    "url": "/user-profile/static/js/runtime~main.95343425.js"
+  },
+  {
+    "revision": "8f7edd16080663ec84ce",
+    "url": "/user-profile/static/js/main.8f7edd16.chunk.js"
+  },
+  {
+    "revision": "76d49e86149da746e90e",
+    "url": "/user-profile/static/js/1.76d49e86.chunk.js"
+  },
+  {
+    "revision": "8f7edd16080663ec84ce",
+    "url": "/user-profile/static/css/main.ba8852c4.chunk.css"
+  },
+  {
+    "revision": "427aa3fdd400bd375058a835a11e24f1",
+    "url": "/user-profile/index.html"
+  }
+];
